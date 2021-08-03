081b04f79ae10c335ad3f91e98abaf1a1b82d962
Cathlyn <cathlynchen18@gmail.com>
2021-08-02 23:15:29 -0400

    Update clock

--- frontend/main.js
+++ frontend/main.js
@@ -33,3 +33,14 @@ function switchMode(mode) {
     updateClock();
 }
 
+function updateClock() {
+    const { remainingTime } = timer;
+    const minutes = `${remainingTime.minutes}`.padStart(2, '0');
+    const seconds = `${remainingTime.seconds}`.padStart(2, '0');
+
+    const min = document.getElementById('js-minutes');
+    const sec = document.getElementById('js-seconds');
+    min.textContent = minutes;
+    sec.textContent = seconds;
+}
+
