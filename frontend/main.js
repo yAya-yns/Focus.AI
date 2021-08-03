13eec40861fe522daab8bb282ba8568f1911f6f7
Cathlyn <cathlynchen18@gmail.com>
2021-08-02 23:14:26 -0400

    Added button function

--- frontend/main.js
+++ frontend/main.js
@@ -4,3 +4,14 @@ const timer = {
     longBreak: 15,
     longBreakInterval: 4,
 };
+
+const modeButtons = document.querySelector('#js-mode-buttons');
+modeButtons.addEventListener('click', handleMode);
+
+function handleMode(event) {
+    const { mode } = event.target.dataset;
+
+    if (!mode) return;
+
+    switchMode(mode);
+}
