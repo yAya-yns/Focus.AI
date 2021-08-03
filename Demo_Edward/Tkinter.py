5fe4720b5ac272f95eca02d7b81db5d055db9c08
Chunlin_Li <2744355699@qq.com>
2021-08-03 10:57:23 -0400

    tkinter part is revised

--- Demo_Edward/Tkinter.py
+++ Demo_Edward/Tkinter.py
@@ -1,20 +1,17 @@
-# import tkinter as tk
-#
-# root = tk.Tk()
-# T = tk.Text(root, height=2, width=30)
+import tkinter as tk
+root = tk.Tk()
+T = tk.Text(root, height=2, width=30)
+T.pack()
+T.insert(tk.END, "Just a text Widget\nin two lines\n")
+tk.mainloop()
+
+
+# win = tk.Tk()
+# # win.geometry("600x250")
+# T = tk.Text(win, height=2, width=30)
 # T.pack()
 # T.insert(tk.END, "Just a text Widget\nin two lines\n")
-# tk.mainloop()
-
-#Import the tkinter library
-from tkinter import *
-
-#Create an instance of tkinter frame
-win = Tk()
-
-#Set the geometry
-win.geometry("600x250")
-
-win.eval('tk::PlaceWindow . center')
-
-win.mainloop()
+#
+# win.eval('tk::PlaceWindow . center')
+#
+# win.mainloop()
