f1af9a55f66e88d7275c591e7f6c73a9a68b903d
Cathlyn <cathlynchen18@gmail.com>
2021-08-02 23:56:25 -0400

    Updated main

--- frontend/main.js
+++ frontend/main.js
@@ -6,18 +6,99 @@ const timer = {
     sessions: 0,
 };
 
+let interval;
+
+const buttonSound = new Audio('button-sound.mp3');
+const mainButton = document.getElementById('js-btn');
+mainButton.addEventListener('click', () => {
+    buttonSound.play();
+    const { action } = mainButton.dataset;
+    if (action === 'start') {
+        startTimer();
+    } else {
+        stopTimer();
+    }
+});
+
 const modeButtons = document.querySelector('#js-mode-buttons');
 modeButtons.addEventListener('click', handleMode);
 
-function handleMode(event) {
-    const { mode } = event.target.dataset;
+function getRemainingTime(endTime) {
+    const currentTime = Date.parse(new Date());
+    const difference = endTime - currentTime;
 
-    if (!mode) return;
+    const total = Number.parseInt(difference / 1000, 10);
+    const minutes = Number.parseInt((total / 60) % 60, 10);
+    const seconds = Number.parseInt(total % 60, 10);
 
-    switchMode(mode);
+    return {
+        total,
+        minutes,
+        seconds,
+    };
+}
 
-    stopTimer();
+function startTimer() {
+    let { total } = timer.remainingTime;
+    const endTime = Date.parse(new Date()) + total * 1000;
 
+    if (timer.mode === 'pomodoro') timer.sessions++;
+
+    mainButton.dataset.action = 'stop';
+    mainButton.textContent = 'stop';
+    mainButton.classList.add('active');
+
+    interval = setInterval(function () {
+        timer.remainingTime = getRemainingTime(endTime);
+        updateClock();
+
+        total = timer.remainingTime.total;
+        if (total <= 0) {
+            clearInterval(interval);
+
+            switch (timer.mode) {
+                case 'pomodoro':
+                    if (timer.sessions % timer.longBreakInterval === 0) {
+                        switchMode('longBreak');
+                    } else {
+                        switchMode('shortBreak');
+                    }
+                    break;
+                default:
+                    switchMode('pomodoro');
+            }
+
+            document.querySelector(`[data-sound="${timer.mode}"]`).play();
+
+            startTimer();
+        }
+    }, 1000);
+}
+
+function stopTimer() {
+    clearInterval(interval);
+
+    mainButton.dataset.action = 'start';
+    mainButton.textContent = 'start';
+    mainButton.classList.remove('active');
+}
+
+function updateClock() {
+    const { remainingTime } = timer;
+    const minutes = `${remainingTime.minutes}`.padStart(2, '0');
+    const seconds = `${remainingTime.seconds}`.padStart(2, '0');
+
+    const min = document.getElementById('js-minutes');
+    const sec = document.getElementById('js-seconds');
+    min.textContent = minutes;
+    sec.textContent = seconds;
+
+    const text =
+        timer.mode === 'pomodoro' ? 'Get back to work!' : 'Take a break!';
+    document.title = `${minutes}:${seconds} — ${text}`;
+
+    const progress = document.getElementById('js-progress');
+    progress.value = timer[timer.mode] * 60 - timer.remainingTime.total;
 }
 
 function switchMode(mode) {
@@ -40,107 +121,15 @@ function switchMode(mode) {
     updateClock();
 }
 
-function updateClock() {
-    const { remainingTime } = timer;
-    const minutes = `${remainingTime.minutes}`.padStart(2, '0');
-    const seconds = `${remainingTime.seconds}`.padStart(2, '0');
+function handleMode(event) {
+    const { mode } = event.target.dataset;
 
-    const min = document.getElementById('js-minutes');
-    const sec = document.getElementById('js-seconds');
-    min.textContent = minutes;
-    sec.textContent = seconds;
+    if (!mode) return;
 
-    const text = timer.mode === 'pomodoro' ? 'Get back to work!' : 'Take a break!';
-    document.title = `${minutes}:${seconds} — ${text}`;
-
-    const progress = document.getElementById('js-progress');
-    progress.value = timer[timer.mode] * 60 - timer.remainingTime.total;
+    switchMode(mode);
+    stopTimer();
 }
 
-let interval;
-
-const buttonSound = new Audio('button-sound.mp3');
-const mainButton = document.getElementById('js-btn');
-mainButton.addEventListener('click', () => {
-    buttonSound.play();
-    const { action } = mainButton.dataset;
-    if (action === 'start') {
-        startTimer();
-    } else {
-        stopTimer();
-    }
+document.addEventListener('DOMContentLoaded', () => {
+    switchMode('pomodoro');
 });
-
-function startTimer() {
-    let { total } = timer.remainingTime;
-    const endTime = Date.parse(new Date()) + total * 1000;
-
-    if (timer.mode === 'pomodoro') timer.sessions++;
-
-    interval = setInterval(function () {
-        timer.remainingTime = getRemainingTime(endTime);
-        updateClock();
-
-        total = timer.remainingTime.total;
-        if (total <= 0) {
-            clearInterval(interval);
-            switch (timer.mode) {
-                case 'pomodoro':
-                    if (timer.sessions % timer.longBreakInterval === 0) {
-                        switchMode('longBreak');
-                    } else {
-                        switchMode('shortBreak');
-                    }
-                    break;
-                default:
-                    switchMode('pomodoro');
-            }
-        }
-
-        document.querySelector(`[data-sound="${timer.mode}"]`).play();
-
-        startTimer();
-    }, 1000);
-}
-
-function getRemainingTime(endTime) {
-    const currentTime = Date.parse(new Date());
-    const difference = endTime - currentTime;
-
-    const total = Number.parseInt(difference / 1000, 10);
-    const minutes = Number.parseInt((total / 60) % 60, 10);
-    const seconds = Number.parseInt(total % 60, 10);
-
-    return {
-        total,
-        minutes,
-        seconds,
-    };
-}
-
-const mainButton = document.getElementById('js-btn');
-
-mainButton.addEventListener('click', () => {
-    const { action } = mainButton.dataset;
-    if (action === 'start') {
-        startTimer();
-
-    } else {
-
-        stopTimer();
-
-    }
-
-});
-
-
-
-function stopTimer() {
-    clearInterval(interval);
-
-    mainButton.dataset.action = 'start';
-    mainButton.textContent = 'start';
-    mainButton.classList.remove('active');
-}
-
-
