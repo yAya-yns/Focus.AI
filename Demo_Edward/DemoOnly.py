8e4d26dd492cb46b5be8a7c04e946c2fa124c4ca
Chunlin_Li <2744355699@qq.com>
2021-08-03 01:32:25 -0400

    a new dataset is added

--- Demo_Edward/DemoOnly.py
+++ Demo_Edward/DemoOnly.py
@@ -5,21 +5,23 @@ Check the README.md for complete documentation.
 
 import cv2
 from gaze_tracking import GazeTracking
+import pyautogui
+pyautogui.FAILSAFE
 import time
 
 gaze = GazeTracking()
 webcam = cv2.VideoCapture(0)
 left_bound = 0.7
-right_bound = 0.46
-lower_bound = 0.81
-upper_bound = 0.89
+right_bound = 0.55
+lower_bound = 0.75
+upper_bound = 0.84
 h_diff = left_bound - right_bound
 v_diff = upper_bound - lower_bound
 
 v_buffer = []
 h_buffer = []
 
-for i in range(25):
+for i in range(20):
     _, frame = webcam.read()
     height = frame.shape[0]
     width = frame.shape[1]
@@ -49,6 +51,7 @@ while True:
     hr = gaze.horizontal_ratio()
     vr = gaze.vertical_ratio()
     if hr is None or vr is None:
+        cv2.imshow("Demo", frame)
         continue
     v_buffer.append(vr)
     h_buffer.append(hr)
@@ -56,12 +59,13 @@ while True:
     h_arg = sum(h_buffer)/len(h_buffer)
     # print(v_arg, h_arg)
     # box_center = [100, 100]
-    box_center = [width * abs(h_arg - left_bound) / h_diff, height * abs(v_arg - upper_bound) / v_diff]
+    box_center = [width * abs(h_arg - left_bound) / h_diff, height * abs(v_arg - lower_bound) / v_diff]
+    pyautogui.moveTo(box_center[0], box_center[1])
     print(box_center)
-    top_left_corner = (int(box_center[0]) - 40, int(box_center[1]) - 40)
-    lower_right_corner = (int(box_center[0]) + 40, int(box_center[1]) + 40)
+    # top_left_corner = (int(box_center[0]) - 40, int(box_center[1]) - 40)
+    # lower_right_corner = (int(box_center[0]) + 40, int(box_center[1]) + 40)
 
-    cv2.rectangle(frame, top_left_corner, lower_right_corner, (255, 0, 0), 2)
+    # cv2.rectangle(frame, top_left_corner, lower_right_corner, (255, 0, 0), 2)
 
     v_buffer.pop(0)
     h_buffer.pop(0)
