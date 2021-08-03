d5e563d6ea19b23b191ae939a6af124744d2d184
Cathlyn <cathlynchen18@gmail.com>
2021-08-02 23:32:35 -0400

    Tab display

--- frontend/main.js
+++ frontend/main.js
@@ -50,6 +50,9 @@ function updateClock() {
     min.textContent = minutes;
     sec.textContent = seconds;
 
+    const text = timer.mode === 'pomodoro' ? 'Get back to work!' : 'Take a break!';
+    document.title = `${minutes}:${seconds} — ${text}`;
+
     const progress = document.getElementById('js-progress');
     progress.value = timer[timer.mode] * 60 - timer.remainingTime.total;
 }
