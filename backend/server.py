c3690f805a8ccfb95a9ee6059d7bb965b93dec41
wuwangzhang1216 <60851473+wuwangzhang1216@users.noreply.github.com>
2021-08-04 01:43:20 -0400

    add package images

--- backend/server.py
+++ backend/server.py
@@ -2,14 +2,18 @@ from flask import Flask, render_template
 import os
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
   return render_template('index.html')
 
+
 @app.route('/my-link/')
 def my_link():
+  os.system("echo stevejobs")
   os.system("python3 main.py")
   return 'The eye-tracking is started!'
+
 
 if __name__ == '__main__':
   app.run(debug=True)
