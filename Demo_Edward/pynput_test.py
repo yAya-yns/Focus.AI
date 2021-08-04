287460c93fefaa18ab125e8d7c8b1ccf88a1cfd4
Chunlin_Li <2744355699@qq.com>
2021-08-04 09:37:59 -0400

    hot key added

---
+++ Demo_Edward/pynput_test.py
@@ -0,0 +1,15 @@
+from pynput import keyboard
+
+def on_activate():
+    print('Global hotkey activated!')
+
+def for_canonical(f):
+    return lambda k: f(l.canonical(k))
+
+hotkey = keyboard.HotKey(
+    keyboard.HotKey.parse('<cmd>+;'),
+    on_activate)
+with keyboard.Listener(
+        on_press=for_canonical(hotkey.press),
+        on_release=for_canonical(hotkey.release)) as l:
+    l.join()
